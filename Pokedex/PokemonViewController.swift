689dbc0edbcd8f6d6796a4d255a6920d7b97b42f
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-11 10:03:22 -0600

    Setup PokemonViewController

---
+++ Pokedex/PokemonViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Bradley GIlmore on 4/11/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var abilitiesLabel: UILabel!
+}
